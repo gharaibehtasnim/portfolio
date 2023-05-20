5483ba2e35d6acbcab9f4cc3b285983ba30dbdf8
Tasneem Gharaibeh <tasnim.gharaibeh@gmail.com>
2023-05-20 22:38:40 +0300

    final

--- src/constants/index.js
+++ src/constants/index.js
@@ -122,7 +122,7 @@ import {
       ],
     },
     {
-      title: "React Native Developer",
+      title: "React js Developer",
       company_name: "Tesla",
       icon: tesla,
       iconBg: "#E6DEDD",
